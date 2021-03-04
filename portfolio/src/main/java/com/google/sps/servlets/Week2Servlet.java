1ccbfdc442cd3fd8d10ed85ab4035aa52ed2274e
Michael <mjalawes@gmail.com>
2021-03-04 19:57:28 +0000

    Change messages to Arrays.asList and format pom.xml dependencies

--- portfolio/src/main/java/com/google/sps/servlets/Week2Servlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/Week2Servlet.java
@@ -3,6 +3,7 @@ package com.google.sps.servlets;
 import com.google.gson.Gson;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,17 +13,16 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/week-2")
 public class Week2Servlet extends HttpServlet {
 
-	List<String> messages = new ArrayList<String>();
-
+	List<String> messages = Arrays.asList(
+			"Maybe you received this message first",
+			"Maybe you received this message second",
+			"Maybe you received this message third",
+			"Maybe you received this message last",
+			"Maybe you received no messages at all"
+			);
+	
 	@Override
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-
-		// Add hardcoded messages to ArrayList
-		messages.add("Maybe you received this message first");
-		messages.add("Maybe you received this message second");
-		messages.add("Maybe you received this message third");
-		messages.add("Maybe you received this message last");
-		messages.add("Maybe you received no messages at all");
 
 		// Convert hard-coded messages to JSON
 		Gson gson = new Gson();
