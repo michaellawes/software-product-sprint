cc1adb31ae8f6c9eacc4d73c9eae35bd6b029534
Michael <mjalawes@gmail.com>
2021-03-04 00:26:29 +0000

    Fix error with servlet class

--- portfolio/src/main/java/com/google/sps/servlets/Week2Servlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/Week2Servlet.java
@@ -1,7 +1,9 @@
 package com.google.sps.servlets;
 
-import java.io.IOException;
 import com.google.gson.Gson;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -10,14 +12,18 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/week-2")
 public class Week2Servlet extends HttpServlet {
 
-	List<String> messages = new ArrayList<>();
-	messages.add("Maybe you received this message first");
-	messages.add("Maybe you received this message second");
-	messages.add("Maybe you received this message third");
-	messages.add("Maybe you received this message last");
-	messages.add("Maybe you received no messages at all");
+	List<String> messages = new ArrayList<String>();
+
 	@Override
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+		// Add hardcoded messages to ArrayList
+		messages.add("Maybe you received this message first");
+		messages.add("Maybe you received this message second");
+		messages.add("Maybe you received this message third");
+		messages.add("Maybe you received this message last");
+		messages.add("Maybe you received no messages at all");
+
 		// Convert hard-coded messages to JSON
 		Gson gson = new Gson();
 		String json = gson.toJson(messages);
