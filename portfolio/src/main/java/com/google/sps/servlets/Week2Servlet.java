ddb123654eda00ac164e33ffcc0e6854658492cf
Michael <mjalawes@gmail.com>
2021-03-04 00:07:41 +0000

    Modify servlet to return ArrayList as JSON string

--- portfolio/src/main/java/com/google/sps/servlets/Week2Servlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/Week2Servlet.java
@@ -1,6 +1,7 @@
 package com.google.sps.servlets;
 
 import java.io.IOException;
+import com.google.gson.Gson;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -17,7 +18,12 @@ public class Week2Servlet extends HttpServlet {
 	messages.add("Maybe you received no messages at all");
 	@Override
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		// Convert hard-coded messages to JSON
+		Gson gson = new Gson();
+		String json = gson.toJson(messages);
+
+		// Send the JSON as the response
 		response.setContentType("text/html;");
-		response.getWriter().println("<h1>hello world</h1>");
+		response.getWriter().println(json);
 	}	
 }
