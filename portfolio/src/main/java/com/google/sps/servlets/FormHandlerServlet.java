9654ffe13b8e64be04f91ab614003d3dfda9a0ad
Michael <mjalawes@gmail.com>
2021-03-09 17:22:03 +0000

    Format FormHandlerServlet.java indentation

--- portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
@@ -1,6 +1,6 @@
 package com.google.sps.servlets;
 
-import java.io.IOException;i
+import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -10,8 +10,8 @@ import javax.servlet.http.HttpServletResponse;
 public class FormHandlerServlet extends HttpServlet {
 
 	@Override
-	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-				    
+	public void doPost(HttpServletRequest request, HttpServletResponse response) 
+			throws IOException {		    
 		// Get the value entered in the form.	
 		 String name = request.getParameter("name");
 		 String email = request.getParameter("email");
@@ -19,10 +19,12 @@ public class FormHandlerServlet extends HttpServlet {
 		 String text = request.getParameter("text");
 
 		// Print the value so you can see it in the server logs.
-		System.out.println("Recevied " + subject +  " from " + name + " at " + email + " saying " + text);
+		System.out.println("Recevied " + subject +  " from " + name 
+					+ " at " + email + " saying " + text);
 
 		// Write the value to the response so the user can see it.
-		response.getWriter().println("You submitted " + subject + " successfully!");
+		response.getWriter().println("You submitted " + subject 
+						+ " successfully!");
 		response.sendRedirect("https://mlawes-sps-spring21.appspot.com");		
 	}
 }
